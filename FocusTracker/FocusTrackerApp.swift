579937c5d4528fd5eeb00812a5393d1994610266
nanthi13 <94906368+nanthi13@users.noreply.github.com>
2026-01-20 15:23:57 +0100

    initial commit

---
+++ FocusTracker/FocusTrackerApp.swift
@@ -0,0 +1,12 @@
+//CREATED  BY: nanthi13ON 20/01/2026
+
+import SwiftUI
+
+@main
+struct FocusTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FocusTrackerTests/FocusTrackerTests.swift
@@ -0,0 +1,12 @@
+//CREATED  BY: nanthi13ON 20/01/2026
+
+import Testing
+@testable import FocusTracker
+
+struct FocusTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
