ef754f25f7b914fee9739ac0f15639a8ea9ffb19
nanthi13 <94906368+nanthi13@users.noreply.github.com>
2026-07-27 15:22:20 +0200

    feat: add pastMonth filter for TaskHistoryView

--- FocusTracker/Views/History/TaskHistoryView.swift
+++ FocusTracker/Views/History/TaskHistoryView.swift
@@ -22,6 +22,7 @@ struct TaskHistoryView: View {
         case all = "All"
         case today = "Today"
         case week = "This Week"
+        case pastMonth = "Past Month"
 
         var id: String { rawValue }
     }
@@ -40,6 +41,13 @@ struct TaskHistoryView: View {
             if let startOfWeek = calendar.dateInterval(of: .weekOfYear, for: Date())?.start,
                let endOfWeek = calendar.date(byAdding: .day, value: 7, to: startOfWeek) {
                 items = items.filter { $0.date >= startOfWeek && $0.date < endOfWeek }
+            }
+        case .pastMonth:
+            let startOfThisWeek = calendar.dateInterval(of: .weekOfYear, for: Date())?.start
+            if let startOfThisWeek = startOfThisWeek,
+                let startOfLastWeek = calendar.date(byAdding: .day, value: -7, to: startOfThisWeek),
+                let endOfLastWeek = calendar.date(byAdding: .day, value: 7, to: startOfLastWeek) {
+                items = items.filter { $0.date >= startOfLastWeek && $0.date < endOfLastWeek }
             }
         }
 
